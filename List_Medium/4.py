bf136c5b1a3141e8c89be2bf8d1c7ea32b583e13
manovijayaraghavan <manovijayaraghavan@gmail.com>
2026-06-03 12:25:11 +0530

    Delete List_Medium directory

--- List_Medium/4.py
+++
@@ -1,8 +0,0 @@
-# Question: Write a Python program for List Medium problem 4.
-
-n = int(input())
-arr =  list(map(int,input().split()))
-k = int(input())
-rest = arr[:-k]
-last = arr[-k:]
-print(last + rest)
