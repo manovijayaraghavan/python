903b6ba0551a31214f3c6316bf9f4db43485f178
manovijayaraghavan <manovijayaraghavan@gmail.com>
2026-06-03 11:55:18 +0530

    Add files via upload

---
+++ Input and Output/10.py
@@ -0,0 +1,8 @@
+dead = int(input())
+injured = int(input())
+safe = int(input())
+
+print("1)Dead:", dead, sep="")
+print("2)Injured:", injured, sep="")
+print("3)Safe:", safe, sep="")
+print("Please help the people who are suffering!!!")
